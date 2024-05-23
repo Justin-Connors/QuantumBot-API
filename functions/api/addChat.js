4c20f0dd192ecb5dde67d8e91381d1ef2b4c5023
Justin <justinconnors@hotmail.ca>
2024-05-23 16:17:01 -0300

    addChat function working

--- functions/api/addChat.js
+++ functions/api/addChat.js
@@ -4,7 +4,34 @@ const { logger } = functions;
 
 exports.addChat = functions.https.onCall(async (data, context) => {
   try {
+    logger.log('Received request to add chat:', data);
 
+    if(!data.userId) {
+      logger.log('Required fields userId is missing:', data);
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'userId is required'
+      );
+    }
+
+    const { userId } = data;
+
+    const chatData = {
+      userId,
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    const chatRef = await admin
+      .firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('chats')
+      .add(chatData);
+
+    logger.log('Chat added:', chatRef.id);
+
+    return { status: 'success', chatId: chatRef.id };
+      
   } catch(e) {
     logger.error(e);
       throw new functions.https.HttpsError(
