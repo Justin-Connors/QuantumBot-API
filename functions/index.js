a70db5e09d53517cdda566371e37d0528e26a243
Justin <justinconnors@hotmail.ca>
2024-05-23 15:07:15 -0300

    added exports in index, removed un-needed api endpoints

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,15 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 // Function imports
+const {addChat} = require('./api/addChat');
+const {endChat} = require('./api/endChat');
+const {deleteChat} = require('./api/deleteChat');
+const {deleteMessage} = require('./api/deleteMessage');
+const {addMessage} = require('./api/addMessage');
 
 // Function exports
+exports.addChat = addChat;
+exports.endChat = endChat;
+exports.deleteChat = deleteChat;
+exports.deleteMessage = deleteMessage;
+exports.addMessage = addMessage;
